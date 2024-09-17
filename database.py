52d66801db6c65979f563800e74928e7c97cc448
mimi-202 <135345898+mimi-202@users.noreply.github.com>
2024-09-17 19:09:27 +0800

    Add files via upload

---
+++ database.py
@@ -0,0 +1,35 @@
+#database 
+
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Float, Boolean
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# SQL Sever login credentials/connection string
+SQLALCHEMY_DATABASE_URL = "mssql+pyodbc://trackerUser:ecu2024@MIREILLE\\SQLEXPRESS/boatTracker?driver=ODBC+Driver+17+for+SQL+Server"
+
+#connection and interaction to database
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+base = declarative_base()
+
+# Definition of BoatDetails model that will map to the 'boatDetails' table in the database
+class BoatDetails(base):
+    __tablename__ = "boatDetails"
+
+    boatID = Column(Integer, primary_key=True, index=True)  
+    boatModel = Column(String)  
+    launchTime = Column(DateTime)  
+    retrievalTime = Column(DateTime, nullable=True) 
+    timeAtSea = Column(Float, nullable= True) 
+    isOrphan = Column (Boolean, default= True)
+
+# Function that allows connection to the database and ensures it is closed after use 
+def get_db():
+    db = SessionLocal() 
+    try:
+        yield db
+    finally:
+        db.close()
+
+
